ee97d53503e2ca3ccf1534b3690f891814e7c3c4
nnanna217 <nnanna217@gmail.com>
2025-06-04 11:57:12 +0100

    Initial Commit

---
+++ IOSSmileWebView/IOSSmileWebViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOSSmileWebViewApp.swift
+//  IOSSmileWebView
+//
+//  Created by Nnanna Eze on 04/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct IOSSmileWebViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
